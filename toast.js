84d08e65b4625c52bac653b9c615c2fc6b4970c0
Dennis Kwon <dennis@tappytoon.com>
2022-07-28 16:07:17 +0900

    좀더 객체스럽게 하기위해 반환 타입을 리터럴 형태로 변경

--- toast.js
+++ toast.js
@@ -1,17 +1,29 @@
-var toast = (function () {
+var toast = function () {
     let button = document.createElement('div')
-    button.style.border = 'thick solid #0000FF'
-    button.style.borderRadius = '1px'
-    button.style.position = 'fixed'
-    button.style.backgroundColor = 'red'
-    document.body.appendChild(button)
-    
-    return function setAttribute(width, height, left, bottom) {
+
+    function render(width, height, left, bottom) {
+        button.style.border = 'thick solid #0000FF'
+        button.style.borderRadius = '1px'
+        button.style.position = 'fixed'
+        button.style.backgroundColor = 'red'
+
         button.style.left = left
         button.style.bottom = bottom
         button.style.width = width
         button.style.height = height
+        document.body.appendChild(button)
     }
-})()
+    
+    return { 
+        init(width, height, left, bottom) {
+            render(width, height, left, bottom)
+        },
+        update(show) {
+            button.hidden = show
+        }
+    }
+}
 
-toast('200px', '50px', '20px', '20px')
+let toastCompoent = toast()
+toastCompoent.init('200px', '50px', '20px', '20px')
+toastCompoent.update(true)
