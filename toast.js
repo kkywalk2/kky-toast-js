07cd7c3f8b579751bec7addfd8bbb1aefb484b23
Dennis Kwon <dennis@tappytoon.com>
2022-09-05 16:53:04 +0900

    timeout으로 queue 구현, 리팩토링 및 동시성 관련 처리 필요

--- toast.js
+++ toast.js
@@ -1,12 +1,23 @@
 const Toast = function () {
     let button = document.createElement('div')
     let queue = []
+    let timeoutId = undefined
 
     function _fadeOut() {
+        if(timeoutId) clearTimeout(timeoutId)
         button.style.visibility = "hidden"
+        setTimeout(_popQueue, 100)
     }
 
-    function _init(width, height, left, bottom, content) {
+    function _popQueue() {
+        if(queue.length) {
+            let data = queue.shift()
+            _show(data.content)
+            timeoutId = setTimeout(_fadeOut, data.timeout)
+        }
+    }
+
+    function _init(width, height, left, bottom) {
         button.style.border = 'thick solid #0000FF'
         button.style.borderRadius = '1px'
         button.style.position = 'fixed'
@@ -16,24 +27,34 @@ const Toast = function () {
         button.style.bottom = bottom
         button.style.width = width
         button.style.height = height
-        button.textContent = content
         button.style.textAlign = "center"
         button.style.lineHeight = height
         button.style.visibility = "hidden"
+        button.addEventListener("click", _fadeOut);
         document.body.appendChild(button)
     }
 
-    function _show() {
+    function _show(content) {
+        button.textContent = content
         button.style.visibility = "visible"
     }
     
     return { 
-        init(width, height, left, bottom, content) {
-            _init(width, height, left, bottom, content)
+        init(width, height, left, bottom) {
+            _init(width, height, left, bottom)
+        },
+        add(content, timeout) {
+            queue.push({
+                content : content,
+                timeout : timeout
+            })
         },
         show() {
-            _show()
-            setTimeout(_fadeOut, 1000)
+            let data = queue.shift()
+            if(data) {
+                _show(data.content)
+                timeoutId = setTimeout(_fadeOut, data.timeout)
+            }
         }
     }
 }
